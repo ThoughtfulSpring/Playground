1c3d260896b5b4f6fb82f62586571c5acd0ad23e
Niiaou <xnhoang+git@gmail.com>
2020-02-22 18:26:03 +0100

    feat: remove presenter for mvvm architecture

--- app/src/main/java/e/niiaouhdev/playground/feature/form/FormActivity.kt
+++ app/src/main/java/e/niiaouhdev/playground/feature/form/FormActivity.kt
@@ -4,30 +4,29 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import e.niiaouhdev.playground.R
 import e.niiaouhdev.playground.databinding.FormActivityBinding
 import e.niiaouhdev.playground.feature.form.model.FormViewModel
 
-class FormActivity : AppCompatActivity(), FormPresenter.FormPresenterView {
+class FormActivity : AppCompatActivity() {
 
     private lateinit var binding: FormActivityBinding
     private lateinit var model: FormViewModel
-    private lateinit var presenter: FormPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
        binding = DataBindingUtil.setContentView(this, R.layout.form_activity)
        model = ViewModelProviders.of(this)[FormViewModel::class.java]
-       presenter = FormPresenter(this, model)
 
         binding.lifecycleOwner = this
         binding.model = model
-        binding.presenter = presenter
+
     }
 
-    override fun showSuccessMessage() {
+    private fun showSuccessMessage() {
         Toast.makeText(this, "Save successful !", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/e/niiaouhdev/playground/feature/form/model/FormViewModel.kt
+++ app/src/main/java/e/niiaouhdev/playground/feature/form/model/FormViewModel.kt
@@ -3,6 +3,9 @@ package e.niiaouhdev.playground.feature.form.model
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import e.niiaouhdev.playground.feature.utils.uiThread
+import java.util.*
+import kotlin.concurrent.schedule
 
 class FormViewModel : ViewModel() {
     val title = MutableLiveData<String>()
@@ -19,5 +22,15 @@ class FormViewModel : ViewModel() {
         isLoading.value = false
         isSavedButtonEnabled.addSource(title) { isSavedButtonEnabled.value = !title.value.isNullOrBlank() && isLoading.value != true }
         isSavedButtonEnabled.addSource(isLoading) { isSavedButtonEnabled.value = !title.value.isNullOrBlank() && isLoading.value != true }
+    }
+
+    fun save() {
+        isLoading.value = true
+
+        Timer().schedule(3000) {
+            uiThread {
+                isLoading.value = false
+            }
+        }
     }
 }
